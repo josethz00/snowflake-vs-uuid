45af49aa5e919e48950175b69ffd2cc43a90f2c0
josethz00 <jose.soares18@fatec.sp.gov.br>
2024-01-28 14:30:32 -0300

    feat: base CLI

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"time"
+
+	"github.com/fatih/color"
+)
+
 func main() {
-	println("Hello, World!")
+	boldPurple := color.New(color.FgHiMagenta, color.Bold)
+	boldPurple.Println("BENCHMARK: Snowflake vs UUID")
+	time.Sleep(800 * time.Millisecond)
+	boldPurple.Println(".....................................")
+	time.Sleep(1000 * time.Millisecond)
+	boldPurple.Println("=====================================")
+	time.Sleep(600 * time.Millisecond)
+	boldPurple.Println("=====================================")
+	time.Sleep(200 * time.Millisecond)
+	boldPurple.Println(".....................................")
 }
