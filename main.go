07bac8063079b99700d70b68c492549218bf203b
josethz00 <jose.soares18@fatec.sp.gov.br>
2024-01-28 22:23:27 -0300

    feat: benchmark insertions

--- main.go
+++ main.go
@@ -11,9 +11,24 @@ import (
 	"github.com/josethz00/snowflake-vs-uuid/utils"
 )
 
+type TestsUUID struct {
+	id         string
+	name       string
+	created_at time.Time
+	updated_at time.Time
+}
+
+type TestsSnowflake struct {
+	id         int64
+	name       string
+	created_at time.Time
+	updated_at time.Time
+}
+
 func main() {
 	boldPurple := color.New(color.FgHiMagenta, color.Bold)
 	boldPurple.Println("BENCHMARK: Snowflake vs UUID")
+	db := utils.NewDB().ConnectDB()
 
 	snowflakeNode, err := snowflake.NewNode(1)
 	if err != nil {
@@ -69,4 +84,43 @@ func main() {
 	wg.Wait()
 	elapsedTime = time.Since(startTime)
 	boldCyan.Println("Elapsed time for Snowflakes:", elapsedTime)
+
+	// Insert UUIDs into DB
+	boldCyan.Printf("Generating %d UUIDs and inserting them into the DB... \n", numIds)
+	wg.Add(numGoroutines)
+	startTime = time.Now()
+
+	for i := 0; i < numGoroutines; i++ {
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+			for j := 0; j < numIds/numGoroutines; j++ {
+				uuid := utils.GenUUID()
+				db.InsertIntoTestsUUID(uuid, "test")
+			}
+		}(&wg)
+	}
+
+	wg.Wait()
+	elapsedTime = time.Since(startTime)
+	boldCyan.Println("Elapsed time for UUIDs insertion:", elapsedTime)
+
+	// Insert Snowflakes into DB
+	boldCyan.Printf("Generating %d Snowflakes and inserting them into the DB... \n", numIds)
+	wg.Add(numGoroutines)
+	startTime = time.Now()
+
+	for i := 0; i < numGoroutines; i++ {
+		go func(wg *sync.WaitGroup) {
+			defer wg.Done()
+			for j := 0; j < numIds/numGoroutines; j++ {
+				snowfid := utils.GenSnowflake(snowflakeNode)
+				db.InsertIntoTestsSnowflake(snowfid, "test")
+			}
+		}(&wg)
+	}
+
+	wg.Wait()
+	elapsedTime = time.Since(startTime)
+	boldCyan.Println("Elapsed time for Snowflakes insertion:", elapsedTime)
+
 }
