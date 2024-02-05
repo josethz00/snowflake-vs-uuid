b2bcb58c52436260fae18538a7203bbdb68abbd4
josethz00 <jose.soares18@fatec.sp.gov.br>
2024-02-05 20:09:12 -0300

    scripts to run bencmarks 10x

---
+++ run-uuid.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Number of runs
+RUNS=10
+
+# Number of IDs to generate for each run
+NUM_IDS=100000
+
+echo "Running benchmark $RUNS times with $NUM_IDS IDs each..."
+
+for i in $(seq 1 $RUNS); do
+    echo "Run $i..."
+    printf "%d\n" $NUM_IDS | docker exec -i uuid-benchmark ./uuidbenchmark.out
+done
+
+echo "Benchmark completed."
