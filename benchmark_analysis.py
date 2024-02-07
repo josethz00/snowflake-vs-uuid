e95d4aac3d6c35ee7d0c28be7b08063f4e9b7858
josethz00 <jose.soares18@fatec.sp.gov.br>
2024-02-07 19:30:26 -0300

    plot benchmark comparison

--- benchmark_analysis.py
+++ benchmark_analysis.py
@@ -1,26 +1,27 @@
+from typing import Literal, Optional
 import pandas as pd
 import os
 import numpy as np
 from scipy import stats
 from matplotlib import pyplot as plt
+import matplotlib.ticker as mticker
+
+def read_csv_report_files_by_operation(operation: str, benchmark: Literal["snowflake", "uuid"] = "snowflake", dir: Optional[str] = None) -> list:
+    if not dir:
+        dir = os.path.join(os.getcwd(), 'reports', f"{benchmark}benchmark")
+
+    times_ms = []
+
+    for f in os.listdir(dir):
+        if f.endswith('.csv'):
+            df = pd.read_csv(os.path.join(dir, f))
+            times_ms.append(df.loc[df["Operation"] == operation]["ElapsedTime"].values[0])
+
+    return times_ms
 
     
-snowflake_ordered_selection_times_ms = []
-
-# for each file in dir, read CSV and get elapsed time for ordered selection
-for f in os.listdir(os.path.join(os.getcwd(), 'reports', 'snowflakebenchmark')):
-    if f.endswith('.csv'):
-        df = pd.read_csv(os.path.join(os.getcwd(), 'reports', 'snowflakebenchmark', f))
-        snowflake_ordered_selection_times_ms.append(df.loc[df["Operation"] == "Ordered Selection"]["ElapsedTime"].values[0])
-
-
-uuid_ordered_selection_times_ms = []
-
-# for each file in dir, read CSV and get elapsed time for ordered selection
-for f in os.listdir(os.path.join(os.getcwd(), 'reports', 'uuidbenchmark')):
-    if f.endswith('.csv'):
-        df = pd.read_csv(os.path.join(os.getcwd(), 'reports', 'uuidbenchmark', f))
-        uuid_ordered_selection_times_ms.append(df.loc[df["Operation"] == "Ordered Selection"]["ElapsedTime"].values[0])
+snowflake_ordered_selection_times_ms = read_csv_report_files_by_operation("Ordered Selection", "snowflake")
+uuid_ordered_selection_times_ms = read_csv_report_files_by_operation("Ordered Selection", "uuid")
 
 print("ORDERED SELECTION (SNOWFLAKE)")
 print(f"Mean: {np.mean(snowflake_ordered_selection_times_ms)}")
@@ -39,21 +40,9 @@ print(stats.ttest_ind(snowflake_ordered_selection_times_ms, uuid_ordered_selecti
 print("\n =================================== \n")
 
 
-snowflake_generation_times_ms = []
+snowflake_generation_times_ms = read_csv_report_files_by_operation("Generation", "snowflake")
+uuid_generation_times_ms = read_csv_report_files_by_operation("Generation", "uuid")
 
-# for each file in dir, read CSV and get elapsed time for id generation
-for f in os.listdir(os.path.join(os.getcwd(), 'reports', 'snowflakebenchmark')):
-    if f.endswith('.csv'):
-        df = pd.read_csv(os.path.join(os.getcwd(), 'reports', 'snowflakebenchmark', f))
-        snowflake_generation_times_ms.append(df.loc[df["Operation"] == "Generation"]["ElapsedTime"].values[0])
-
-uuid_generation_times_ms = []
-
-# for each file in dir, read CSV and get elapsed time for id generation
-for f in os.listdir(os.path.join(os.getcwd(), 'reports', 'uuidbenchmark')):
-    if f.endswith('.csv'):
-        df = pd.read_csv(os.path.join(os.getcwd(), 'reports', 'uuidbenchmark', f))
-        uuid_generation_times_ms.append(df.loc[df["Operation"] == "Generation"]["ElapsedTime"].values[0])
 
 print("ID GENERATION (SNOWFLAKE)")
 print(f"Mean: {np.mean(snowflake_generation_times_ms)}")
@@ -72,21 +61,9 @@ print(stats.ttest_ind(snowflake_generation_times_ms, uuid_generation_times_ms, e
 print("\n =================================== \n")
 
 
-snowflake_insertion_times_ms = []
+snowflake_insertion_times_ms = read_csv_report_files_by_operation("Generation + Insertion", "snowflake")
+uuid_insertion_times_ms = read_csv_report_files_by_operation("Generation + Insertion", "uuid")
 
-# for each file in dir, read CSV and get elapsed time for insertion
-for f in os.listdir(os.path.join(os.getcwd(), 'reports', 'snowflakebenchmark')):
-    if f.endswith('.csv'):
-        df = pd.read_csv(os.path.join(os.getcwd(), 'reports', 'snowflakebenchmark', f))
-        snowflake_insertion_times_ms.append(df.loc[df["Operation"] == "Generation + Insertion"]["ElapsedTime"].values[0])
-
-uuid_insertion_times_ms = []
-
-# for each file in dir, read CSV and get elapsed time for insertion
-for f in os.listdir(os.path.join(os.getcwd(), 'reports', 'uuidbenchmark')):
-    if f.endswith('.csv'):
-        df = pd.read_csv(os.path.join(os.getcwd(), 'reports', 'uuidbenchmark', f))
-        uuid_insertion_times_ms.append(df.loc[df["Operation"] == "Generation + Insertion"]["ElapsedTime"].values[0])
 
 print("INSERTION (SNOWFLAKE)")
 print(f"Mean: {np.mean(snowflake_insertion_times_ms)}")
@@ -105,21 +82,9 @@ print(stats.ttest_ind(snowflake_insertion_times_ms, uuid_insertion_times_ms, equ
 print("\n =================================== \n")
 
 
-snowflake_selection_times_ms = []
+snowflake_selection_times_ms = read_csv_report_files_by_operation("Selection", "snowflake")
+uuid_selection_times_ms = read_csv_report_files_by_operation("Selection", "uuid")
 
-# for each file in dir, read CSV and get elapsed time for selection
-for f in os.listdir(os.path.join(os.getcwd(), 'reports', 'snowflakebenchmark')):
-    if f.endswith('.csv'):
-        df = pd.read_csv(os.path.join(os.getcwd(), 'reports', 'snowflakebenchmark', f))
-        snowflake_selection_times_ms.append(df.loc[df["Operation"] == "Selection"]["ElapsedTime"].values[0])
-
-uuid_selection_times_ms = []
-
-# for each file in dir, read CSV and get elapsed time for selection
-for f in os.listdir(os.path.join(os.getcwd(), 'reports', 'uuidbenchmark')):
-    if f.endswith('.csv'):
-        df = pd.read_csv(os.path.join(os.getcwd(), 'reports', 'uuidbenchmark', f))
-        uuid_selection_times_ms.append(df.loc[df["Operation"] == "Selection"]["ElapsedTime"].values[0])
 
 print("SELECTION (SNOWFLAKE)")
 print(f"Mean: {np.mean(snowflake_selection_times_ms)}")
@@ -138,21 +103,9 @@ print(stats.ttest_ind(snowflake_selection_times_ms, uuid_selection_times_ms, equ
 print("\n =================================== \n")
 
 
-snowflake_update_times_ms = []
+snowflake_update_times_ms = read_csv_report_files_by_operation("Update", "snowflake")
+uuid_update_times_ms = read_csv_report_files_by_operation("Update", "uuid")
 
-# for each file in dir, read CSV and get elapsed time for update
-for f in os.listdir(os.path.join(os.getcwd(), 'reports', 'snowflakebenchmark')):
-    if f.endswith('.csv'):
-        df = pd.read_csv(os.path.join(os.getcwd(), 'reports', 'snowflakebenchmark', f))
-        snowflake_update_times_ms.append(df.loc[df["Operation"] == "Update"]["ElapsedTime"].values[0])
-
-uuid_update_times_ms = []
-
-# for each file in dir, read CSV and get elapsed time for update
-for f in os.listdir(os.path.join(os.getcwd(), 'reports', 'uuidbenchmark')):
-    if f.endswith('.csv'):
-        df = pd.read_csv(os.path.join(os.getcwd(), 'reports', 'uuidbenchmark', f))
-        uuid_update_times_ms.append(df.loc[df["Operation"] == "Update"]["ElapsedTime"].values[0])
 
 print("UPDATE (SNOWFLAKE)")
 print(f"Mean: {np.mean(snowflake_update_times_ms)}")
@@ -171,21 +124,9 @@ print(stats.ttest_ind(snowflake_update_times_ms, uuid_update_times_ms, equal_var
 print("\n =================================== \n")
 
 
-snowflake_search_by_id_times_ms = []
+snowflake_search_by_id_times_ms = read_csv_report_files_by_operation("Search", "snowflake")
+uuid_search_by_id_times_ms = read_csv_report_files_by_operation("Search", "uuid")
 
-# for each file in dir, read CSV and get elapsed time for search by id
-for f in os.listdir(os.path.join(os.getcwd(), 'reports', 'snowflakebenchmark')):
-    if f.endswith('.csv'):
-        df = pd.read_csv(os.path.join(os.getcwd(), 'reports', 'snowflakebenchmark', f))
-        snowflake_search_by_id_times_ms.append(df.loc[df["Operation"] == "Search"]["ElapsedTime"].values[0])
-
-uuid_search_by_id_times_ms = []
-
-# for each file in dir, read CSV and get elapsed time for search by id
-for f in os.listdir(os.path.join(os.getcwd(), 'reports', 'uuidbenchmark')):
-    if f.endswith('.csv'):
-        df = pd.read_csv(os.path.join(os.getcwd(), 'reports', 'uuidbenchmark', f))
-        uuid_search_by_id_times_ms.append(df.loc[df["Operation"] == "Search"]["ElapsedTime"].values[0])
 
 print("SEARCH BY ID (SNOWFLAKE)")
 print(f"Mean: {np.mean(snowflake_search_by_id_times_ms)}")
@@ -303,4 +244,53 @@ plt.grid(True)
 
 
 plt.savefig("insertion_improved.png", dpi=300)
+plt.show()
+
+
+categories = ("UUID", "Snowflake")
+commands_means = {
+    'Generation': (np.mean(uuid_generation_times_ms) / 1000, np.mean(snowflake_generation_times_ms) / 1000),
+    'INSERT': (np.mean(uuid_insertion_times_ms) /1000, np.mean(snowflake_insertion_times_ms) /1000),
+    'SELECT': (np.mean(uuid_selection_times_ms) /1000, np.mean(snowflake_selection_times_ms) /1000),
+    'SELECT BY ID (WHERE)': (np.mean(uuid_search_by_id_times_ms) /1000, np.mean(snowflake_search_by_id_times_ms) /1000),
+    'SELECT + ORDER BY': (np.mean(uuid_ordered_selection_times_ms) /1000, np.mean(snowflake_ordered_selection_times_ms) /1000),
+    'UPDATE': (np.mean(uuid_update_times_ms) /1000, np.mean(snowflake_update_times_ms) /1000),
+}
+
+
+color_palette = plt.get_cmap('tab10')
+
+fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(18, 10), dpi=100)
+
+plt.subplots_adjust(wspace=0.4, hspace=0.4)
+
+axes = axes.flatten()
+
+for i, (cmd, times) in enumerate(commands_means.items()):
+    ax = axes[i]
+
+    x_max = max(times) * 1.2
+    ax.set_xlim(0, x_max)
+
+    bars = ax.barh(['UUID', 'Snowflake'], times, color=[color_palette(0), color_palette(1)], height=0.5)
+
+    for bar in bars:
+        width = bar.get_width()
+        label_x_pos = width if width < x_max / 10 else width - (x_max / 20)
+        ax.text(label_x_pos + (bar.get_width() * 0.1), bar.get_y() + bar.get_height()/2, f'{width:.2f} s',
+                va='center', ha='left', color='black', fontsize=10, fontweight='bold')
+
+    ax.set_yticks([1, 0])
+    ax.set_yticklabels(['UUID', 'Snowflake'])
+
+    ax.set_title(cmd, fontsize=16, fontweight='bold')
+    ax.set_xlabel('Time (s)', fontsize=14)
+
+    ax.tick_params(axis='both', which='major', labelsize=12)
+
+    ax.grid(True, linestyle='--', linewidth=0.5, alpha=0.7)
+
+fig.tight_layout()
+plt.savefig('benchmark_comparison.png', dpi=300, bbox_inches='tight')
+
 plt.show()
